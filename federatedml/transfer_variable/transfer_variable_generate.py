e285c7d81a630efd7027ebc980b22ad2f4a49989
weiwee <wbwmat@gmail.com>
2019-12-04 17:44:23 +0800

    reformat generator
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- federatedml/transfer_variable/transfer_variable_generate.py
+++ federatedml/transfer_variable/transfer_variable_generate.py
@@ -17,61 +17,112 @@
 #  limitations under the License.
 #
 
-# =============================================================================
-# Transfer Variable Generator.py
-# =============================================================================
-
 import json
 import os
 from collections import OrderedDict
 
-from arch.api.utils import file_utils
+BASE_DIR = os.getcwd()
+SHEBANG = """
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""".strip()
+LICENCE = """
+#
+#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+""".strip()
+TRANSFER_CLASS_TEMPERATE = """
+################################################################################
+#
+# AUTO GENERATED TRANSFER VARIABLE CLASS. DO NOT MODIFY
+#
+################################################################################
 
-BASE_DIR = os.path.join(file_utils.get_project_base_directory(), "federatedml", "transfer_variable")
-TEMPLATE = os.path.join(BASE_DIR, "transfer_variable.template")
-CONF_DIR = os.path.join(BASE_DIR, "definition")
-MERGE_CONF_FILE_NAME = "transfer_conf.json"
-MERGE_CONF_PATH = os.path.join(CONF_DIR, MERGE_CONF_FILE_NAME)
-CLASS_DIR = os.path.join(BASE_DIR, "transfer_class")
-
-SPACES = "    "
+from federatedml.transfer_variable.base_transfer_variable import BaseTransferVariables
 
 
-def write_out_class(writer, class_name, variable_names):
-    def create_variable(name):
-        return f"self.{name} = self._create_variable(name='{name}')"
+# noinspection PyAttributeOutsideInit
+class {class_name}(BaseTransferVariables):
+    def __init__(self, flowid=0):
+        super().__init__(flowid)
+        {create_variable}
+""".strip()
 
-    with open(TEMPLATE, "r") as fin:
-        temp = fin.read()
-        temp = temp.format(class_name=class_name,
-                           create_variable=f"\n{SPACES}{SPACES}".join(map(create_variable, variable_names)))
-        writer.write(temp)
-    writer.flush()
+merge_dict = OrderedDict()  # merged conf in order
+init_file_import = OrderedDict() # import class in order
 
 
-def generate():
-    merge_dict = OrderedDict()  # makes merged conf in order
-
-    for f_name in sorted(os.listdir(CONF_DIR)):
-        if not f_name.endswith(".json") or f_name == MERGE_CONF_FILE_NAME:
+def parse_transfer_variable_conf():
+    conf_dir = os.path.join(os.getcwd(), "definition")
+    for f_name in sorted(os.listdir(conf_dir)):
+        if not f_name.endswith(".json") or f_name == "transfer_conf.json":
             continue
 
-        with open(os.path.join(CONF_DIR, f_name), "r") as fin:
+        with open(os.path.join(conf_dir, f_name), "r") as fin:
             var_dict = json.loads(fin.read())
             merge_dict.update(var_dict)
-
-        name = f_name.split(".")[0]
-        class_save_path = os.path.join(CLASS_DIR, f"{name}_transfer_variable.py")
-        with open(class_save_path, "w") as f:
             keys = list(var_dict.keys())
-            assert len(keys) == 1, "multi class defined in a single json"
+            if len(keys) < 1:
+                continue
+            if len(keys) > 1:
+                raise ValueError("multi class defined in a single json")
             class_name = keys[0]
+            file_name = f_name.split(".")[0]
             variable_names = sorted(var_dict.get(class_name).keys())
-            write_out_class(f, class_name, variable_names)
+            init_file_import[file_name] = class_name
+        yield file_name, class_name, variable_names
+
+
+def generate(with_shebang=True, with_licence=True):
+    for file_name, class_name, variable_names in parse_transfer_variable_conf():
+        class_save_path = os.path.join(os.path.join(BASE_DIR, "transfer_class"), f"{file_name}_transfer_variable.py")
+        with open(class_save_path, "w") as f:
+
+            def create_variable(v_name):
+                return f"self.{v_name} = self._create_variable(name='{v_name}')"
+
+            temp = TRANSFER_CLASS_TEMPERATE.format(
+                class_name=class_name,
+                create_variable=f"\n        ".join(map(create_variable, variable_names))
+            )
+            if with_shebang:
+                f.write(SHEBANG)
+                f.write("\n")
+                f.write("\n")
+            if with_licence:
+                f.write(LICENCE)
+                f.write("\n")
+                f.write("\n")
+            f.write(temp)
+            f.write("\n")
+            f.flush()
 
     # save a merged transfer variable conf, for federation auth checking.
-    with open(MERGE_CONF_PATH, "w") as f:
+    with open(os.path.join(BASE_DIR, "definition", "transfer_conf.json"), "w") as f:
         json.dump(merge_dict, f, indent=1)
+
+    with open(os.path.join(os.getcwd(), "__init__.py"), "w") as f:
+        f.write(SHEBANG)
+        f.write("\n")
+        f.write("\n")
+        f.write(LICENCE)
+        f.write("\n")
+        f.write("\n")
+        for name, class_name in init_file_import.items():
+            f.write(f"from .transfer_class.{name}_transfer_variable import {class_name}")
+            f.write("\n")
 
 
 if __name__ == "__main__":
