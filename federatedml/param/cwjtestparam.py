c705f874dad40dc66ce67e2406353e6cbe89c2e7
chenweijing <talkingwallace@sohu.com>
2020-02-19 12:01:25 +0800

    complete secure

--- federatedml/param/cwjtestparam.py
+++
@@ -1,9 +0,0 @@
-from federatedml.param.base_param import BaseParam
-
-class CwjParam(BaseParam):
-
-    def __init__(self,test=None):
-        self.test = test
-
-    def check(self):
-        print('hello world')
